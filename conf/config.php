0c48c309b26db8f4a07aa5f5b4f046d8bbf1fedc
traaxz38 <117074424+traaxz38@users.noreply.github.com>
2023-04-27 20:50:23 +0200

    Add files via upload
    commit de sécu

--- conf/config.php
+++ conf/config.php
@@ -4,9 +4,9 @@
 session_start();
 
 $pdo = new PDO(
-    'mysql:host=mysql-matisbarbierdelaunay.alwaysdata.net;dbname=matisbarbierdelaunay_bigproject;',
-    '298920',
-    'SFb65ADFSURify5',
+    'mysql:host=localhost;dbname=bigproject;',
+    'root',
+    '',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
